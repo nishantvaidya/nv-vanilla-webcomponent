732acbec026a4345ee2b480aff1fe670bff01bd0
nishantvaidya <nishant.mca2005@gmail.com>
2020-09-23 13:16:29 +0530

    star

--- nv-star-rating.js
+++ nv-star-rating.js
@@ -22,73 +22,73 @@ class NvStarRating extends HTMLElement {
   connectedCallback(){
     this._root.innerHTML = `
      <style>
-      :host{
-        width:4.1em;
+      :host {
+        width: 4.1em;
         height: 1em;
-        display:inline-block;
+        display: inline-block;
         overflow: hidden;
         user-select: none;
         vertical-align: middle;
         box-sizing: border-box;
       }
        .container {                  
-                  color: #c5c5c5;
-                  font-size: 1em;
-                  line-height: 1em;
-                  margin: 0 auto;
-                  position: relative;
-                  padding: 0;
-                  cursor: pointer;
-                }               
-                .container .top {
-                  color: #e7bd06;
-                  padding: 0;
-                  position: absolute;
-                  z-index: 1;
-                  display: block;
-                  top: 0;
-                  left: 0;
-                  overflow: hidden;
-                  width: 0;       
-                }
-                .container:hover .top {
-                    display: none;
-                }                             
-                .container .bottom {
-                  padding: 0;
-                  display: block;
-                  position: absolute;
-                  top: 0;
-                  left: 0;
-                  unicode-bidi: bidi-override;
-                  direction: rtl;
-                }
-                /* Credit: https://css-tricks.com/star-ratings/ */
-                .container .bottom > span:hover,
-                .container .bottom > span:hover ~ span {               
-                   color: #e7bd06;
-                }               
-                :host([disabled]) .container {
-                    cursor: inherit;
-                }
-                :host([disabled]) .container .top {
-                    display: block;
-                }
-                :host([disabled]) .container .bottom > span:hover,
-                :host([disabled]) .container .bottom > span:hover ~ span {
-                    color: inherit;
-                }
+          color: #c5c5c5;
+          font-size: 1em;
+          line-height: 1em;
+          margin: 0 auto;
+          position: relative;
+          padding: 0;
+          cursor: pointer;
+        }               
+      .container .top {
+        color: #e7bd06;
+        padding: 0;
+        position: absolute;
+        z-index: 1;
+        display: block;
+        top: 0;
+        left: 0;
+        overflow: hidden;
+        width: 0;       
+      }
+        .container:hover .top {
+            display: none;
+        }                             
+        .container .bottom {
+          padding: 0;
+          display: block;
+          position: absolute;
+          top: 0;
+          left: 0;
+          unicode-bidi: bidi-override;
+          direction: rtl;
+        }
+        /* Credit: https://css-tricks.com/star-ratings/ */
+        .container .bottom > span:hover,
+        .container .bottom > span:hover ~ span {               
+            color: #e7bd06;
+        }               
+        :host([disabled]) .container {
+            cursor: inherit;
+        }
+        :host([disabled]) .container .top {
+            display: block;
+        }
+        :host([disabled]) .container .bottom > span:hover,
+        :host([disabled]) .container .bottom > span:hover ~ span {
+            color: inherit;
+        }
      </style>
      <div class="container">
        <div class="top">
-        <span>*</span><span>*</span><span>*</span><span>*</span><span>*</span>
+        <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
        </div>
        <div class="bottom">
-         <span data-value="5">*</span>
-         <span data-value="4">*</span>
-         <span data-value="3">*</span>
-         <span data-value="2">*</span>
-         <span data-value="1">*</span>
+         <span data-value="5">★</span>
+         <span data-value="4">★</span>
+         <span data-value="3">★</span>
+         <span data-value="2">★</span>
+         <span data-value="1">★</span>
        </div>
      </div>
     `;
@@ -112,9 +112,9 @@ class NvStarRating extends HTMLElement {
   }
 
   _render(){
-    if(this._$top !== null){
-      this._$top.style.width = ((this._value * 10) *2 ) + "%";
-    }
+       if (this._$top !== null) {
+            this._$top.style.width = ((this._value * 10) * 2) + "%";
+        }
   }
 
   static get observedAttributes(){
